c93e734c0da5f4ee302e2ab1d142dc28e51460a5
Randunu Kaushan <randunukaushan9@gmail.com>
2026-09-19 04:45:35 +0530

    feat: enable Experience slideshow in CMS

--- sanity/schemaTypes/heroCarouselType.ts
+++ sanity/schemaTypes/heroCarouselType.ts
@@ -13,6 +13,7 @@ export const heroCarouselType = defineType({
       options: {
         list: [
           { title: "Home", value: "home" },
+          { title: "Experience", value: "experiences" },
           { title: "Dining", value: "dining" },
           { title: "Gallery", value: "gallery" },
           { title: "Kandy", value: "kandy" },
