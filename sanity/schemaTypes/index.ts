94df93697fb414450f0a63bad75a225c6b7e647d
Randunu Kaushan <randunukaushan9@gmail.com>
2026-09-19 03:16:02 +0530

    feat: add sanity/schemaTypes/index.ts

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,23 @@
+import { roomType } from "@/sanity/schemaTypes/roomType";
+import { siteSettingsType } from "@/sanity/schemaTypes/siteSettingsType";
+import {
+  attractionType,
+  diningType,
+  experienceType,
+  faqType,
+  galleryItemType,
+  offerType,
+  reviewType,
+} from "@/sanity/schemaTypes/supportingTypes";
+
+export const schemaTypes = [
+  siteSettingsType,
+  roomType,
+  experienceType,
+  diningType,
+  offerType,
+  faqType,
+  reviewType,
+  galleryItemType,
+  attractionType,
+];
