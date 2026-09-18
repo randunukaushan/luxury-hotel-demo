f6e37dd7a4d92e25c82aa12c844d5b1c177b62bc
Randunu Kaushan <randunukaushan9@gmail.com>
2026-09-19 03:17:40 +0530

    build: allow Sanity image CDN

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
